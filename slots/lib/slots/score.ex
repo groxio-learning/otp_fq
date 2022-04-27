c9c9c803b6e579f7d8446ef50ca0fbadeea54825
7ganam@gmail.com <7ganam@gmail.com>
2022-04-27 20:18:34 +0200

    refactored board

--- slots/lib/slots/score.ex
+++ slots/lib/slots/score.ex
@@ -3,26 +3,32 @@ defmodule Slots.Score do
 	#def new, do: __struct__()
 
 	def new(answer, guess) do
-		reds = 
-			answer 
-			|> Enum.zip(guess)
-			|> Enum.filter(fn {x, y} -> x == y end)
-			|> length
 
-		not_in_list = 
-		 (answer -- guess)
-		 |> Enum.count
+		reds = reds(answer, guess)
 
-		total = length(answer)
-
-		whites = total - reds - not_in_list
+		whites = total(answer) - reds - not_in_list(answer, guess)
 
 		__struct__(white: whites, red: reds)
 
 	end
 
+	defp total(answer), do:  length(answer)
 
-	def show_score(%{ white: white, red: red }) do
+	defp not_in_list(answer, guess) do
+		(answer -- guess)
+		|> Enum.count
+	end
+
+	defp reds(answer,guess) do
+		reds =
+			answer
+			|> Enum.zip(guess)
+			|> Enum.count(fn {x, y} -> x == y end)
+	end
+
+	def render(%{ white: white, red: red }) do
 		String.duplicate("R", red) <> String.duplicate("W", white)
 	end
+
+	def show_score(answer, guess), do: new(answer, guess) |> render()
 end
