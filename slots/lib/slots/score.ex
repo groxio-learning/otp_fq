a974fc564f0e2c9597034d7b1feb08d2b5989ec4
7ganam@gmail.com <7ganam@gmail.com>
2022-04-27 23:02:39 +0200

    added render_status

--- slots/lib/slots/score.ex
+++ slots/lib/slots/score.ex
@@ -1,34 +1,23 @@
 defmodule Slots.Score do
 	defstruct white: 0, red: 0
 	#def new, do: __struct__()
-
 	def new(answer, guess) do
-
 		reds = reds(answer, guess)
-
 		whites = total(answer) - reds - not_in_list(answer, guess)
-
 		__struct__(white: whites, red: reds)
-
 	end
-
-	defp total(answer), do:  length(answer)
-
+	defp total(answer), do: length(answer)
 	defp not_in_list(answer, guess) do
-		(answer -- guess)
-		|> Enum.count
+		(answer -- guess) |> Enum.count
 	end
-
 	defp reds(answer,guess) do
 		reds =
 			answer
 			|> Enum.zip(guess)
 			|> Enum.count(fn {x, y} -> x == y end)
 	end
-
 	def render(%{ white: white, red: red }) do
 		String.duplicate("R", red) <> String.duplicate("W", white)
 	end
-
 	def show_score(answer, guess), do: new(answer, guess) |> render()
 end
