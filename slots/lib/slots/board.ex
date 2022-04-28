ecce45bfd0ff2652a0cdb27ad868a962f85c7940
Eduardo Borsa <borsa.dado@gmail.com>
2022-04-28 10:55:24 -0300

    Slots GenServer

--- slots/lib/slots/board.ex
+++ slots/lib/slots/board.ex
@@ -1,31 +1,38 @@
 defmodule Slots.Board do
   alias Slots.Score
-  # Enum.map(board.guesses, &render_row/1)
-  # Enum.map(board.guesses, &render_row(answer, &1))
   defstruct solution: [1, 2, 3, 4], guesses: []
+
   def new, do: __struct__()
+
   def random_new() do
-    solution = 1..8
-    |> Enum.shuffle
-    |> Enum.take(4)
+    solution =
+      1..8
+      |> Enum.shuffle()
+      |> Enum.take(4)
+
     %__MODULE__{solution: solution}
   end
-  def add_guess(%{guesses: guesses}=board , guess) do
-    %{ board | guesses: [guess | guesses]}
+
+  def add_guess(%{guesses: guesses} = board, guess) do
+    %{board | guesses: [guess | guesses]}
   end
+
   def render_row(answer, guess) do
-    "#{inspect guess} | #{Score.show_score(answer, guess)}"
+    "#{inspect(guess)} | #{Score.show_score(answer, guess)}"
   end
+
   def render(board) do
     """
     #{render_rows(board)}
     #{render_status(board)}
     """
   end
+
   def render_rows(board) do
     Enum.map(board.guesses, &render_row(board.solution, &1))
     |> Enum.join("\n")
   end
+
   def render_status(board) do
     cond do
       won?(board) -> :won
@@ -33,7 +40,8 @@ defmodule Slots.Board do
       true -> :playing
     end
   end
-  def won?(%{solution: s, guesses: [s|_]}), do: true
+
+  def won?(%{solution: s, guesses: [s | _]}), do: true
   def won?(%{solution: _, guesses: _}), do: false
   def lost?(board), do: length(board.guesses) > 9
 end
